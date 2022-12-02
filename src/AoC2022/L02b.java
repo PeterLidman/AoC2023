8aef8def7838feb5d5269fef97b324376786472d
Peter Lidman <peterlidman@gmail.com>
2022-12-02 18:26:13 +0100

    nya switchsyntaxen motionerades

--- src/AoC2022/L02b.java
+++ src/AoC2022/L02b.java
@@ -4,19 +4,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class L02b {
 	private static List<String> strat;
 
-	public static void run(String ext) {
-		try {
-			strat = Files.readAllLines(Paths.get("./src/AoC2022/L02input" + ext + ".txt")).stream()
-					.collect(Collectors.toList());
-		} catch (IOException e) {
-			System.out.println("Fel vid filimport: " + e);
-		}
-
+	public static void run(String ext) throws IOException {
+		strat = Files.readAllLines(Paths.get("./src/AoC2022/L02input" + ext + ".txt"));
 		int sum = 0;
 
 		for (int a = 0; a < strat.size(); a++) {
@@ -32,35 +25,6 @@ public class L02b {
 			case "C Z" -> 1 + 6;
 			default -> throw new IllegalArgumentException("Unexpected value: " + strat.get(a));
 			};
-//			switch (strat.get(a)) {
-//			case "A X":// rock lose
-//				sum += 3 + 0 ;
-//				break;
-//			case "A Y":// rock draw
-//				sum += 1 + 3;
-//				break;
-//			case "A Z":// rock win
-//				sum += 2 + 6;
-//				break;
-//			case "B X":// paper
-//				sum += 1 + 0;
-//				break;
-//			case "B Y":
-//				sum += 2 + 3;
-//				break;
-//			case "B Z":
-//				sum += 3 + 6;
-//				break;
-//			case "C X":// scissors
-//				sum += 2 + 0;
-//				break;
-//			case "C Y":
-//				sum += 3 + 3;
-//				break;
-//			case "C Z":
-//				sum += 1 + 6;
-//				break;
-//			}
 		}
 		System.out.println("Total score: " + sum);
 	}
