f5a35df7744f3fc433dad9c8496b52ce28c60ac6
Peter Lidman <peterlidman@gmail.com>
2022-12-04 10:28:49 +0100

    assignments overlaps

--- src/AoC2022/Main.java
+++ src/AoC2022/Main.java
@@ -7,8 +7,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 //		L01a.run("1"); L01a.run("0"); L01b.run("1"); L01b.run("0");
 //		L02a.run("1"); L02a.run("0"); L02b.run("1"); L02b.run("0");
-		L03a.run("1"); L03a.run("0"); L03b.run("1"); L03b.run("0");
-//		L04a.run("1"); L04a.run("0"); L04b.run("1"); L04b.run("0");
+//		L03a.run("1"); L03a.run("0"); L03b.run("1"); L03b.run("0");
+		L04a.run("1"); L04a.run("0"); L04b.run("1"); L04b.run("0");
 //		L05a.run("1"); L05a.run("0"); L05b.run("1"); L05b.run("0");
 //		L06a.run("1"); L06a.run("0"); L06b.run("1"); L06b.run("0");
 //		L07a.run("1"); L07a.run("0"); L07b.run("1"); L07b.run("0");
