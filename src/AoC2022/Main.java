596d7b716855c93a879a0121666d67ace33c472e
Peter Lidman <peterlidman@gmail.com>
2022-12-07 19:51:45 +0100

    No space left on device

--- src/AoC2022/Main.java
+++ src/AoC2022/Main.java
@@ -10,8 +10,8 @@ public class Main {
 //		L03a.run("1"); L03a.run("0"); L03b.run("1"); L03b.run("0");
 //		L04a.run("1"); L04a.run("0"); L04b.run("1"); L04b.run("0");
 //		L05a.run("1"); L05a.run("0"); L05b.run("1"); L05b.run("0");
-		L06a.run("1"); L06a.run("0"); L06b.run("1"); L06b.run("0");
-//		L07a.run("1"); L07a.run("0"); L07b.run("1"); L07b.run("0");
+//		L06a.run("1"); L06a.run("0"); L06b.run("1"); L06b.run("0");
+		L07a.run("1");  L07a.run("0"); L07b.run("1"); L07b.run("0");
 //		L08a.run("1"); L08a.run("0"); L08b.run("1"); L08b.run("0");
 //		L09a.run("1"); L09a.run("0"); L09b.run("1"); L09b.run("0");
 //		L10a.run("1"); L10a.run("0"); L10b.run("1"); L10b.run("0");
@@ -23,12 +23,12 @@ public class Main {
 //		L16a.run("0"); L16b.run("0");		
 //		L17a.run("0"); L17b.run("0");
 //		L18a.run("1"); L18a.run("0"); L18b.run("1"); L18b.run("0");
-		
-		//L19a.run("1");
-		//L19a.run("0");
-		//L19b.run("1");
-		//L19b.run("0");
-		
+
+		// L19a.run("1");
+		// L19a.run("0");
+		// L19b.run("1");
+		// L19b.run("0");
+
 //		L20a.run("1");		L20a.run("0");
 //		L20b.run("0");
 //		L21a.run("1");		L21a.run("0");		L21b.run("0");
