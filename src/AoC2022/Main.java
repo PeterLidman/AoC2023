bd4e85c8ac852153c15d02d01fdc08bb3a2029ce
Peter Lidman <peterlidman@gmail.com>
2022-12-09 15:08:04 +0100

    medans bilen laddade

--- src/AoC2022/Main.java
+++ src/AoC2022/Main.java
@@ -12,8 +12,8 @@ public class Main {
 //		L05a.run("1"); L05a.run("0"); L05b.run("1"); L05b.run("0");
 //		L06a.run("1"); L06a.run("0"); L06b.run("1"); L06b.run("0");
 //		L07a.run("1"); L07a.run("0"); L07b.run("1"); L07b.run("0");
-		L08a.run("1"); L08a.run("0"); L08b.run("1"); L08b.run("0");
-//		L09a.run("1"); L09a.run("0"); L09b.run("1"); L09b.run("0");
+//		L08a.run("1"); L08a.run("0"); L08b.run("1"); L08b.run("0");
+		L09a.run("1"); L09a.run("0"); L09b.run("1"); L09b.run("2"); L09b.run("0");
 //		L10a.run("1"); L10a.run("0"); L10b.run("1"); L10b.run("0");
 //		L11a.run("1"); L11a.run("0"); L11b.run("1"); L11b.run("0");
 //		L12a.run("1"); L12a.run("0"); L12b.run("1"); L12b.run("0");
