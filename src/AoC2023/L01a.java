4ea9ca1bc5ebacc7ef02f15e5b005019174ce3ea
Peter Lidman <peterlidman@gmail.com>
2023-12-02 00:17:38 +0100

    mkt krånglig start i år

--- src/AoC2023/L01a.java
+++ src/AoC2023/L01a.java
@@ -9,25 +9,30 @@ import java.util.stream.Collectors;
 public class L01a {
 	private static List<String> cal;
 
-	public static void run(String ext) {
-		try {
-			cal = Files.readAllLines(Paths.get("./src/AoC2023/L01input" + ext + ".txt")).stream()
-					.collect(Collectors.toList());
-		} catch (IOException e) {
-			System.out.println("fel vid filimport: " + e);
-		}
+	public static void run(String ext) throws IOException {
+		cal = Files.readAllLines(Paths.get("./src/AoC2023/L01input" + ext + ".txt")).stream()
+				.collect(Collectors.toList());
 
-		int sum = 0, max = 0;
-		
+		long sum = 0;
+
 		for (int a = 0; a < cal.size(); a++) {
-			if (cal.get(a).equals("")) {
-				max = Math.max(max, sum);
-				sum = 0;
-			} else {
-				sum += Integer.valueOf(cal.get(a));
+			char[] ca = cal.get(a).toCharArray();
+			int v = 0;
+			for (int b = 0; b < ca.length; b++) {
+				if (Character.isDigit(ca[b])) {
+					v += 10 * (ca[b] - '0');
+					break;
+				}
 			}
+			for (int b = ca.length - 1; b >= 0; b--) {
+				if (Character.isDigit(ca[b])) {
+					v += ca[b] - '0';
+					break;
+				}
+			}
+			//System.out.println(v);
+			sum += v;
 		}
-		max = Math.max(max, sum);
-		System.out.println("Max cal= " + max);
+		System.out.println("Sum cal= " + sum);
 	}
 }
--- src/AoC2023/L01b.java
+++ src/AoC2023/L01b.java
@@ -3,34 +3,66 @@ package AoC2023;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class L01b {
 	private static List<String> cal;
 
-	public static void run(String ext) {
-		try {
-			cal = Files.readAllLines(Paths.get("./src/AoC2023/L01input" + ext + ".txt")).stream()
-					.collect(Collectors.toList());
-		} catch (IOException e) {
-			System.out.println("fel vid filimport: " + e);
-		}
+	public static void run(String ext) throws IOException {
+		cal = Files.readAllLines(Paths.get("./src/AoC2023/L01input" + ext + ".txt")).stream()
+				.collect(Collectors.toList());
 
-		int sum = 0;
-		List<Integer> sumList = new ArrayList<>();
+		long sum = 0;
 
 		for (int a = 0; a < cal.size(); a++) {
-			if (cal.get(a).equals("")) {
-				sumList.add(sum);
-				sum = 0;
-			} else {
-				sum += Integer.valueOf(cal.get(a));
+			String newString = "";
+			String oldString = cal.get(a);
+
+			for (int i = 0; i < oldString.length(); i++) {
+				if (Character.isDigit(oldString.charAt(i))) {
+					newString = newString + oldString.substring(i, i + 1);
+				} else {
+					if (oldString.substring(i).startsWith("one")) {
+						newString = newString + "1";
+					} else if (oldString.substring(i).startsWith("two")) {
+						newString = newString + "2";
+					} else if (oldString.substring(i).startsWith("three")) {
+						newString = newString + "3";
+					} else if (oldString.substring(i).startsWith("four")) {
+						newString = newString + "4";
+					} else if (oldString.substring(i).startsWith("five")) {
+						newString = newString + "5";
+					} else if (oldString.substring(i).startsWith("six")) {
+						newString = newString + "6";
+					} else if (oldString.substring(i).startsWith("seven")) {
+						newString = newString + "7";
+					} else if (oldString.substring(i).startsWith("eight")) {
+						newString = newString + "8";
+					} else if (oldString.substring(i).startsWith("nine")) {
+						newString = newString + "9";
+					}
+				}
 			}
+			char[] ca = newString.toCharArray();
+//			System.out.println(ca);
+
+			int v = 0;
+			for (int b = 0; b < ca.length; b++) {
+				if (Character.isDigit(ca[b])) {
+					v += 10 * (ca[b] - '0');
+					break;
+				}
+			}
+			for (int b = ca.length - 1; b >= 0; b--) {
+				if (Character.isDigit(ca[b])) {
+					v += ca[b] - '0';
+					break;
+				}
+			}
+//			System.out.println(v);
+			sum += v;
 		}
-		sumList.add(sum);
-		sumList.sort(null);
-		System.out.println("3Max cal= " + (sumList.get(sumList.size()-3) + sumList.get(sumList.size()-2) + sumList.get(sumList.size()-1))	);
+		System.out.println("Sum cal= " + sum);
 	}
 }
