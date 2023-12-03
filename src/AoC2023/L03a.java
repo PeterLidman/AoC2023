15fcee22f828b6779ef47483efd6a6c86b77d932
Peter Lidman <peterlidman@gmail.com>
2023-12-03 16:46:04 +0100

    third parse problem

--- src/AoC2023/L03a.java
+++ src/AoC2023/L03a.java
@@ -3,33 +3,57 @@ package AoC2023;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class L03a {
-	private static List<String> r;
+	private static List<String> gear;
+	private static Map<Entry<Integer, Integer>, Integer> gn;
+	private static Map<Entry<Integer, Integer>, Character> gs;
 
 	public static void run(String ext) throws IOException {
-		r = Files.readAllLines(Paths.get("./src/AoC2022/L03input" + ext + ".txt"));
+		gear = Files.readAllLines(Paths.get("./src/AoC2023/L03input" + ext + ".txt"));
 		int sum = 0;
+		gn = new HashMap<>();
+		gs = new HashMap<>();
 
-		for (String a : r) {
-			sum += getCommonItemNumber(a);
+		for (int y = 0; y < gear.size(); y++) {
+			for (int x = 0; x < gear.get(y).length(); x++) {
+				char c = gear.get(y).charAt(x);
+				if (Character.isDigit(c)) {
+					String n = "";
+					while (x < gear.get(y).length() && Character.isDigit(gear.get(y).charAt(x))) {
+						n += gear.get(y).charAt(x);
+						x++;
+					}
+					x--;
+					gn.put(Map.entry(x, y), Integer.valueOf(n));
+				} else if (c != '.') {
+					gs.put(Map.entry(x, y), c);
+				}
+			}
+		}
+		for (Entry<Entry<Integer, Integer>, Integer> n : gn.entrySet()) {
+			if (isSymbolClose(n.getKey(), String.valueOf(n.getValue()).length())) {
+				sum += n.getValue();
+			}
 		}
 		System.out.println("Sum: " + sum);
 	}
 
-	static int getCommonItemNumber(String in) {
-		String a = in.substring(0, in.length() / 2);
-		String b = in.substring(in.length() / 2);
-		for (String t : a.split("")) {
-			if (b.contains(t)) {
-				return getPriority(t.charAt(0));
+	private static boolean isSymbolClose(Entry<Integer, Integer> key, int length) {
+		int sx = key.getKey() - length;
+		int sy = key.getValue() - 1;
+		for (int x = sx; x < (sx + length + 2); x++) {
+			for (int y = sy; y < (sy + 3); y++) {
+				if (gs.get(Map.entry(x, y)) != null) {
+					return true;
+				}
 			}
 		}
-		return 1337;
+		return false;
 	}
 
-	static int getPriority(Character in) {
-		return Character.isLowerCase(in) ? in - 'a' + 1 : in - 'A' + 27;
-	}
 }
--- src/AoC2023/L03b.java
+++ src/AoC2023/L03b.java
@@ -3,33 +3,68 @@ package AoC2023;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class L03b {
-	private static List<String> r;
+	private static List<String> gear;
+	private static Map<Entry<Integer, Integer>, Integer> gnum;
+	private static Map<Entry<Integer, Integer>, Character> gsym;
+	private static Map<Entry<Integer, Integer>, Entry<Integer, Integer>> gcon;
 
 	public static void run(String ext) throws IOException {
-		r = Files.readAllLines(Paths.get("./src/AoC2022/L03input" + ext + ".txt"));
+		gear = Files.readAllLines(Paths.get("./src/AoC2023/L03input" + ext + ".txt"));
 		int sum = 0;
+		gnum = new HashMap<>();
+		gsym = new HashMap<>();
+		gcon = new HashMap<>();
 
-		for (int i = 0; i < r.size(); i += 3) {
-			sum += getCommonItemNumber(r.get(i), r.get(i + 1), r.get(i + 2));
+		for (int y = 0; y < gear.size(); y++) {
+			for (int x = 0; x < gear.get(y).length(); x++) {
+				char c = gear.get(y).charAt(x);
+				if (Character.isDigit(c)) {
+					String n = "";
+					while (x < gear.get(y).length() && Character.isDigit(gear.get(y).charAt(x))) {
+						n += gear.get(y).charAt(x);
+						x++;
+					}
+					x--;
+					gnum.put(Map.entry(x, y), Integer.valueOf(n));
+				} else if (c == '*') {
+					gsym.put(Map.entry(x, y), c);
+				}
+			}
+		}
+		for (Entry<Entry<Integer, Integer>, Integer> n : gnum.entrySet()) {
+			isSymbolClose(n.getKey(), n);
+		}
+		for (Entry<Entry<Integer, Integer>, Entry<Integer, Integer>> out : gcon.entrySet()) {
+			for (Entry<Entry<Integer, Integer>, Entry<Integer, Integer>> in : gcon.entrySet()) {
+				if (out.getValue().equals(in.getValue()) && !out.getKey().equals(in.getKey())
+						&& ((out.getKey().getKey() * 140 + out.getKey().getValue()) > (in.getKey().getKey() * 140
+								+ in.getKey().getValue()))) {
+					sum += gnum.get(out.getKey()) * gnum.get(in.getKey());
+				}
+			}
 		}
 		System.out.println("Sum: " + sum);
 	}
 
-	static int getCommonItemNumber(String in1,String in2,String in3) {
-		for (String t : in1.split("")) {
-			if (in2.contains(t) && in3.contains(t)) {
-				return getPriority(t.charAt(0));
+	private static boolean isSymbolClose(Entry<Integer, Integer> key, Entry<Entry<Integer, Integer>, Integer> n) {
+		int length = String.valueOf(n.getValue()).length();
+		int sx = key.getKey() - length;
+		int sy = key.getValue() - 1;
+		for (int x = sx; x < (sx + length + 2); x++) {
+			for (int y = sy; y < (sy + 3); y++) {
+				if (gsym.get(Map.entry(x, y)) != null) {
+					gcon.put(n.getKey(), Map.entry(x, y));
+					return true;
+				}
 			}
 		}
-		return 1337;
+		return false;
 	}
 
-	static int getPriority(Character in) {
-		return Character.isLowerCase(in) ? in - 'a' + 1 : in - 'A' + 27;
-	}
 }
